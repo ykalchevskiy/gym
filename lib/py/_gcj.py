e04372d0bf4bf2a7b79f7d119016264873d58c29
Yan Kalchevskiy <yan.kalchevskiy@gmail.com>
2016-04-16 20:41:27 +0300

    Added templates of main function for gcj.

---
+++ lib/py/_gcj.py
@@ -0,0 +1,20 @@
+"""
+def solve():
+
+
+
+def main():
+    for i in xrange(1, read_int() + 1):
+        write('Case #{}: {}'.format(i, solve()))
+"""
+
+"""
+def solve():
+
+
+
+def main():
+    for i in xrange(1, read_int() + 1):
+        write('Case #{}: '.format(i), end='')
+        solve()
+"""
