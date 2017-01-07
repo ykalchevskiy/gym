3394789d58fa916895bbb95078ded269bd2f1def
Yan Kalchevskiy <yan.kalchevskiy@gmail.com>
2017-01-07 14:24:41 +0300

    Introduce 'cases'

--- Makefile
+++ Makefile
@@ -5,9 +5,11 @@ help:
 	@echo "Usage: 'make <target>' where <target> is one of:"
 	@echo " help        print help"
 	@echo " clean       remove sources"
-	@echo " prepare     create sources from templates"
+	@echo " prepare_cf  create sources from templates for CodeForces"
+	@echo " prepare_cs  create sources from templates with cases"
 	@echo " parse       run Hightail parser in background"
-	@echo " compete     'clean' && 'prepare' && 'parse'"
+	@echo " compete_cf  'clean' && 'prepare_cf' && 'parse'"
+	@echo " compete_cs  'clean' && 'prepare_cs'"
 	@echo " train       create test sources"
 
 .PHONY : help
@@ -18,21 +20,32 @@ clean:
 
 .PHONY : clean
 
-prepare:
+prepare_cf:
 	@for file in $(FILES); do cp lib/cpp/_template.cpp src/$$file.cpp; done
 	@for file in $(FILES); do cp lib/py/_template.py src/$$file.py; done
 
-.PHONY : prepare
+.PHONY : prepare_cf
+
+prepare_cs:
+	@for file in $(FILES); do cp lib/cpp/_template.cpp src/$$file.cpp; done
+	@for file in $(FILES); do cp lib/py/_template_cases.py src/$$file.py; done
+
+.PHONY : prepare_cs
 
 parse:
 	@(cd bin/ && java -jar Hightail-v0.9.4.jar)&
 
 .PHONY : parse
 
-compete: clean prepare parse
+compete_cf: clean prepare_cf parse
 	@echo Good luck!
 
-.PHONY : compete
+.PHONY : compete_cf
+
+compete_cs: clean prepare_cs
+	@echo Good luck!
+
+.PHONY : compete_cs
 
 train: clean
 	@cp lib/cpp/_template.cpp src/main.cpp
---
+++ lib/py/_template_cases.py
@@ -0,0 +1,92 @@
+from __future__ import division, print_function
+
+import itertools
+import sys
+
+
+def solve():
+    """Solve."""
+
+
+def main():
+    for i in range(1, read_int() + 1):
+        write('Case #{}: {}'.format(i, solve()))
+        # write('Case #{}: '.format(i), end='')
+        # solve()
+
+
+def bye(message=None):
+    if message is not None:
+        write(message)
+    sys.exit()
+
+
+def times(n):
+    return itertools.repeat(None, n)
+
+
+def read(func=None):
+    a = sys.stdin.readline().rstrip('\n')
+    return a if func is None else func(a)
+
+
+def read_array(func=None, sep=None, max_split=-1):
+    array = read().split(sep, max_split) if sep != '' else list(read())
+    return array if func is None else [func(a) for a in array]
+
+
+def read_2d_array(n, func=None, sep=None, max_split=-1):
+    return [read_array(func, sep, max_split) for _ in times(n)]
+
+
+def read_int():
+    """:rtype: int"""
+    return read(int)
+
+
+def read_int_array(sep=None, max_split=-1):
+    """:rtype: list[int]"""
+    return read_array(int, sep, max_split)
+
+
+def read_int_2d_array(n, sep=None, max_split=-1):
+    """:rtype: list[list[int]]"""
+    return read_2d_array(n, int, sep, max_split)
+
+
+def write(*args, **kwargs):
+    sep = kwargs.get('sep', ' ')
+    end = kwargs.get('end', '\n')
+    sys.stdout.write(sep.join(str(a) for a in args) + end)
+
+
+def write_array(array, **kwargs):
+    write(*array, **kwargs)
+
+
+def write_2d_array(array, **kwargs):
+    [write_array(a, **kwargs) for a in array]
+
+
+def _main_():
+    name = ''  # fill with a name of a problem
+    names = False  # fill with True
+    if name or names:
+        in_name = (name + '.in') if name else 'input.txt'
+        out_name = (name + '.out') if name else 'output.txt'
+        stdin = sys.stdin
+        stdout = sys.stdout
+        sys.stdin = open(in_name)
+        sys.stdout = open(out_name, 'w')
+        try:
+            main()
+        finally:
+            sys.stdin.close()
+            sys.stdout.close()
+            sys.stdin = stdin
+            sys.stdout = stdout
+    else:
+        main()
+
+
+_main_()
