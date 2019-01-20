0d35c9edfed4a23c6161b5141997c82531ed0430
Yan Kalchevskiy <superman@yandex-team.ru>
2019-01-20 12:43:09 +0300

    Add segment tree with interval methods

--- lib/py/segment_tree.py
+++ lib/py/segment_tree.py
@@ -55,3 +55,46 @@ class SegmentTree(object):
             else:
                 self._inc(v * 2 + 1, tm + 1, tr, index, value)
             self.tree[v] = self.func(self.tree[v * 2], self.tree[v * 2 + 1])
+
+
+class SegmentTreeEx(object):
+    def __init__(self, array):
+        self.array = array
+        self.tree = [0] * 4 * len(array)
+        self._build(1, 0, len(array) - 1)
+
+    def _build(self, v, l, r):
+        if l == r:
+            self.tree[v] = self.array[l]
+        else:
+            m = (l + r) // 2
+            self._build(v * 2, l, m)
+            self._build(v * 2 + 1, m + 1, r)
+
+    def query(self, l, r):
+        return self._query(1, 0, len(self.array) - 1, l, r)
+
+    def _query(self, v, tl, tr, l, r):
+        if l > r:
+            return 0
+        if tl == tr:
+            return self.tree[v]
+        else:
+            tm = (tl + tr) // 2
+            return self.tree[v] + max([
+                self._query(v * 2, tl, tm, l, min(r, tm)),
+                self._query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r)
+            ])
+
+    def inc(self, l, r, value):
+        self._inc(1, 0, len(self.array) - 1, l, r, value)
+
+    def _inc(self, v, tl, tr, l, r, value):
+        if l > r:
+            return
+        if l == tl and r == tr:
+            self.tree[v] += value
+        else:
+            tm = (tl + tr) // 2
+            self._inc(v * 2, tl, tm, l, min(r, tm), value)
+            self._inc(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r, value)
